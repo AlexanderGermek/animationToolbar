6690ed618e26950bfaae0735b22a28d5c64b7fe8
Alexander Germek <AGGermek@sberbank.ru>
2022-08-05 13:43:05 +0300

    Initial Commit

---
+++ AnimationToolbar/AnimationToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationToolbarApp.swift
+//  AnimationToolbar
+//
+//  Created by Гермек Александр Георгиевич on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
